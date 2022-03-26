0e4aa43217f2e5d0efdb5c3f5525c1ddfb245c0c
eduardojabes <eduardojabes@hotmail.com>
2022-03-26 17:48:57 -0300

    feat: storage setup

---
+++ cmd/storage-service/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"context"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	<-ctx.Done()
+
+}
